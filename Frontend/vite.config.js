5aa5dfb2246cf57eba9f7e8d97affb1652935962
vrishabh081 <rishabhvishwakarma398@gmail.com>
2023-07-23 16:53:52 +0530

    aded

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  root: path.resolve(__dirname, 'src'),
-  build: {
-    outDir: '../dist'
-  },
-  server: {
-    port: 8080
-  }
+  plugins: [react()]
 })
